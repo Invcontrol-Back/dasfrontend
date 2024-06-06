24c0cb659ad779c63d399153eb2ff758698793fc
Daniel <andsanchez017@gmail.com>
2024-06-05 23:11:30 -0500

    Arreglo software

--- InvControlFront/src/app/dashboard/page/inventario/inv-software/inv-software.component.ts
+++ InvControlFront/src/app/dashboard/page/inventario/inv-software/inv-software.component.ts
@@ -29,9 +29,9 @@ export class InvSoftwareComponent {
   data:any[] = []
   metaDataColumns:MetaDataColumn[] = [
     {field:"sof_nombre", title:"NOMBRE"},
-    {field:"sof_version", title:"VERSION"},
+    {field:"sof_version", title:"VERSIÓN"},
     {field:"sof_tipo", title:"TIPO"},
-    {field:"sof_duracion", title:"DURACION"},
+    {field:"sof_duracion", title:"DURACIÓN"},
     {field:"sof_descripcion", title:"DESCRIPCION"},
     {field:"tip_ubi_nombre", title:"LABORATORIO"}
   ]
