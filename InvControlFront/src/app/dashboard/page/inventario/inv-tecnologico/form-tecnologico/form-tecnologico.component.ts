bd292b096a3cc7707ca6ff6e3348c24f95dce997
Katerin2001 <kcecen6749@uta.edu.ec>
2024-06-24 23:54:33 -0500

    cambios componentes
    cambios interfaz y control tecnologicos

--- InvControlFront/src/app/dashboard/page/inventario/inv-tecnologico/form-tecnologico/form-tecnologico.component.ts
+++ InvControlFront/src/app/dashboard/page/inventario/inv-tecnologico/form-tecnologico/form-tecnologico.component.ts
@@ -20,124 +20,145 @@ import { MatSelectChange } from '@angular/material/select';
 export class FormTecnologicoComponent {
   formulario!: FormGroup;
 
-  titulo=''
-  tec= ''
-  dataTipoUbicacion:any[] = []
-  dataEncargado:any[] = []
-  dataCategoria:any[] = []
-  dataDependencia:any[]=[]
-  dataMarca:any[]=[]
-  dataBloque:any[]=[]
-  dataUbicacion:any[] =[]
+  titulo = '';
+  tec = '';
+  dataTipoUbicacion: any[] = [];
+  dataEncargado: any[] = [];
+  dataCategoria: any[] = [];
+  dataDependencia: any[] = [];
+  dataMarca: any[] = [];
+  dataBloque: any[] = [];
+  dataUbicacion: any[] = [];
 
-  constructor(private refrencia:MatDialogRef<FormTecnologicoComponent>,@Inject(MAT_DIALOG_DATA) public data:any,
-private entidadLocalizacion:LocalizacionService,private entidadUsuario:UsuarioService,private entidadCategoria:CategoriaService,
-private entidadDependencia:DependenciaService,private entidadTecnologico:TecnologicoService,private entidadMarca:MarcaService,
-private entidadBloque:BloqueService,private entidadUbicacion:UbicacionService){
-   this.titulo=data?'EDICION':'NUEVO'
-   this.tec=data?data.cat_nombre + ' '+ data.tec_codigo:''
-   this.loadLocalizaciones()
-   this.loadUsuarios()
-   this.loadCategorias()
-   this.loadDependencias()
-   this.loadMarcas()
-   this.loadBloques()
+  constructor(
+    private refrencia: MatDialogRef<FormTecnologicoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private entidadLocalizacion: LocalizacionService,
+    private entidadUsuario: UsuarioService,
+    private entidadCategoria: CategoriaService,
+    private entidadDependencia: DependenciaService,
+    private entidadTecnologico: TecnologicoService,
+    private entidadMarca: MarcaService,
+    private entidadBloque: BloqueService,
+    private entidadUbicacion: UbicacionService
+  ) {
+    this.titulo = data ? 'EDICION' : 'NUEVO';
+    this.tec = data ? data.cat_nombre + ' ' + data.tec_codigo : '';
+    this.loadLocalizaciones();
+    this.loadUsuarios();
+    this.loadCategorias();
+    this.loadDependencias();
+    this.loadMarcas();
+    this.loadBloques();
   }
- ngOnInit(): void {
-  this.cargarFormulario();
-  if (this.titulo == "EDICION"){
-    this.formulario.get('tec_anio_ingreso')?.disable();
-    this.formulario.get('tec_codigo')?.disable();
-  }else{
-    this.formulario.get('tec_anio_ingreso')?.enable();
-    this.formulario.get('tec_codigo')?.enable();
-  }
- }
 
- cargarFormulario(){
-  this.formulario= new FormGroup({
-    tec_id: new FormControl(this.data?.tec_id),
-    tec_codigo:new FormControl(this.data ? this.data.tec_codigo:'', Validators.required),
-    tec_descripcion:new FormControl(this.data ? this.data.tec_descripcion:''),
-    tec_serie:new FormControl(this.data ? this.data.tec_serie:'', Validators.required),
-    tec_modelo:new FormControl(this.data ? this.data.tec_modelo:'', Validators.required),
-    tec_ip:new FormControl(this.data ? this.data.tec_ip:''),
-    tec_anio_ingreso:new FormControl(this.data ? this.data.tec_anio_ingreso:'', Validators.required),
-    tec_encargado:new FormControl(this.data ? this.data.tec_encargado:'', Validators.required),
-    tec_loc:new FormControl(this.data ? this.data.tec_loc:'', Validators.required),
-    tec_cat:new FormControl(this.data ? this.data.tec_cat:'', Validators.required),
-    tec_dep:new FormControl(this.data ? this.data.tec_dep:'', Validators.required),
-    tec_mar:new FormControl(this.data ? this.data.tec_mar:'', Validators.required),
-  })
- }
-  grabar(){
-   const form=this.formulario.getRawValue();
-   this.refrencia.close(form)    
+  ngOnInit(): void {
+    this.cargarFormulario();
+    if (this.titulo === 'EDICION') {
+      this.formulario.get('tec_anio_ingreso')?.disable();
+      this.formulario.get('tec_codigo')?.disable();
+    } else {
+      this.formulario.get('tec_anio_ingreso')?.enable();
+      this.formulario.get('tec_codigo')?.enable();
+    }
   }
-  
-  loadLocalizaciones(){
+
+  cargarFormulario() {
+    this.formulario = new FormGroup({
+      tec_id: new FormControl(this.data?.tec_id),
+      tec_codigo: new FormControl(this.data ? this.data.tec_codigo : '', Validators.required),
+      tec_descripcion: new FormControl(this.data ? this.data.tec_descripcion : ''),
+      tec_serie: new FormControl(this.data ? this.data.tec_serie : '', Validators.required),
+      tec_modelo: new FormControl(this.data ? this.data.tec_modelo : '', Validators.required),
+      tec_ip: new FormControl(this.data ? this.data.tec_ip : ''),
+      tec_anio_ingreso: new FormControl(this.data ? this.data.tec_anio_ingreso : '', [Validators.required, Validators.pattern('^[0-9]*$')]),
+      tec_encargado: new FormControl(this.data ? this.data.tec_encargado : '', Validators.required),
+      tec_loc: new FormControl(this.data ? this.data.tec_loc : '', Validators.required),
+      tec_cat: new FormControl(this.data ? this.data.tec_cat : '', Validators.required),
+      tec_dep: new FormControl(this.data ? this.data.tec_dep : '', Validators.required),
+      tec_mar: new FormControl(this.data ? this.data.tec_mar : '', Validators.required)
+    });
+  }
+
+  grabar() {
+    const form = this.formulario.getRawValue();
+    this.refrencia.close(form);
+  }
+
+  loadLocalizaciones() {
     this.entidadLocalizacion.loadLocalizaciones().subscribe(data => {
-      this.dataTipoUbicacion = data
-    },error => {
+      this.dataTipoUbicacion = data;
+    }, error => {
       // console.log(error)
-    })
+    });
   }
 
-  loadBloques(){
+  onlyNumberKey(event: KeyboardEvent) {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      event.preventDefault();
+      return false;
+    }
+    return true;
+  }
+
+  loadBloques() {
     this.entidadBloque.loadBuildings().subscribe(data => {
-      this.dataBloque = data
-    },error => {
+      this.dataBloque = data;
+    }, error => {
       //console.log(error)
-    })
+    });
   }
+
   onSelectionChangeBloque(event: MatSelectChange) {
-    this.loadFiltroBloques(event.value)
+    this.loadFiltroBloques(event.value);
   }
+
   onSelectionChangeUbicacion(event: MatSelectChange) {
-    this.loadFiltroUbicacion(event.value)
+    this.loadFiltroUbicacion(event.value);
   }
 
-  loadFiltroBloques(filtroBloque:any){
-    this.entidadUbicacion.loadFilterLocationBuildings(filtroBloque).subscribe(data=>{
-      this.dataUbicacion = data
-    })
+  loadFiltroBloques(filtroBloque: any) {
+    this.entidadUbicacion.loadFilterLocationBuildings(filtroBloque).subscribe(data => {
+      this.dataUbicacion = data;
+    });
   }
 
-  loadFiltroUbicacion(filtroUbicacion:any){
-    this.entidadLocalizacion.loadFilterLocalizacionesLaboratorio(filtroUbicacion).subscribe(data=>{
-      this.dataTipoUbicacion = data
-    })
+  loadFiltroUbicacion(filtroUbicacion: any) {
+    this.entidadLocalizacion.loadFilterLocalizacionesLaboratorio(filtroUbicacion).subscribe(data => {
+      this.dataTipoUbicacion = data;
+    });
   }
 
-  loadMarcas(){
+  loadMarcas() {
     this.entidadMarca.loadMarcas().subscribe(data => {
-      this.dataMarca = data
-    },error => {
-      console.log(error)
-    })
+      this.dataMarca = data;
+    }, error => {
+      console.log(error);
+    });
   }
 
-  loadUsuarios(){
+  loadUsuarios() {
     this.entidadUsuario.loadUsuarios().subscribe(data => {
-      this.dataEncargado = data
-    },error => {
-      console.log(error)
-    })
+      this.dataEncargado = data;
+    }, error => {
+      console.log(error);
+    });
   }
-  loadCategorias(){
+
+  loadCategorias() {
     this.entidadCategoria.loadCategoriasEspecificas('TECNOLOGICO').subscribe(data => {
-      this.dataCategoria = data
-    },error => {
-      console.log(error)
-    })
+      this.dataCategoria = data;
+    }, error => {
+      console.log(error);
+    });
   }
 
-  loadDependencias(){
+  loadDependencias() {
     this.entidadDependencia.loadDependencias().subscribe(data => {
-      this.dataDependencia = data
-    },error => {
-      console.log(error)
-    })
+      this.dataDependencia = data;
+    }, error => {
+      console.log(error);
+    });
   }
-
 }
